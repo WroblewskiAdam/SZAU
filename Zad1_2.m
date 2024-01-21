b8eb525cd1f841ffa0d3a5301db2140136d83866
Kacper Marchlewicz <115720673+YoshidoSan@users.noreply.github.com>
2024-01-21 15:01:20 +0100

    sprawko i ez

--- Zad1_2.m
+++ Zad1_2.m
@@ -4,8 +4,9 @@ close all;
 
 func = 1; %1 - Ackley, 2 - Himmelblau
 
+disp('========Particle Swarm========')
 options = optimoptions('particleswarm', 'PlotFcn', 'pswplotbestf');
-
+values1 = [];
 for i = 1:10
 	if func == 1
 		x = particleswarm(@(X) Ackley2(X), 2, [-5, -5], [5, 5], options);
@@ -16,5 +17,21 @@ for i = 1:10
 	end
 	disp(x)
 	disp(y)
-	
+    values1 = [values1; [y,x]];
+end
+
+disp('========Genetic Algoritm========')
+options2 = optimoptions('ga', 'PlotFcn', 'gaplotbestf');
+values2 = [];
+for i = 1:10
+	if func == 1
+		x2 = ga(@(X) Ackley2(X), 2, [],[],[],[], [-5, -5], [5, 5], [],[], options2);
+		y2 = Ackley2(x2);
+	elseif func == 2
+		x2 = ga(@(X) Himmelblau2(X), 2, [],[],[],[], [-5, -5], [5, 5], [],[], options2);
+		y2 = Himmelblau2(x2);
+	end
+	disp(x2)
+	disp(y2)
+    values2 = [values2; [y,x]];
 end
